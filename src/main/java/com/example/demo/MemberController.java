0facf539c6df148a107e2c11e45d1429b2c29e4e
cooperkai <eatkaikai@gmail.com>
2023-07-01 15:08:07 +0800

    crud: update and delete member

--- src/main/java/com/example/demo/MemberController.java
+++ src/main/java/com/example/demo/MemberController.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -37,5 +39,32 @@ public class MemberController {
 		} else {
 			return ResponseEntity.ok(memberPoOpt.get());
 		}
+	}
+	
+	@PatchMapping("/members/{id}")
+	public ResponseEntity<MemberPo> updateMemberHeight(@PathVariable Long id, @RequestBody MemberPo memberReq) {
+		// 查 -> 改-> 存
+		Optional<MemberPo> memberPoOpt = memberRepository.findById(id);
+		if (memberPoOpt.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		MemberPo memberPo = memberPoOpt.get();
+		memberPo.setHeight(memberReq.getHeight());
+		
+		return ResponseEntity.ok(memberRepository.save(memberPo));
+	}
+	
+	@DeleteMapping("/members/{id}")
+	public ResponseEntity<Void> deleteMemberById(@PathVariable Long id) {
+		// 查 -> 刪除
+		Optional<MemberPo> memberPoOpt = memberRepository.findById(id);
+		if (memberPoOpt.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		}
+		
+		memberRepository.deleteById(id);
+		
+		return ResponseEntity.noContent().build();
 	}
 }
