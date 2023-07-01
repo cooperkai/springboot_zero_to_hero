e47b36a1eaa63cad68e758053487a1e0477ff8f6
cooperkai <eatkaikai@gmail.com>
2023-07-01 14:28:44 +0800

    crud: find member and add config to show parameters

--- src/main/java/com/example/demo/MemberController.java
+++ src/main/java/com/example/demo/MemberController.java
@@ -1,6 +1,11 @@
 package com.example.demo;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,5 +22,20 @@ public class MemberController {
 	@PostMapping("/members")
 	public ResponseEntity<MemberPo> postMamber(@RequestBody MemberPo po) {
 		return ResponseEntity.ok(memberRepository.save(po));
+	}
+
+	@GetMapping("/members")
+	public ResponseEntity<List<MemberPo>> findAllMambers() {
+		return ResponseEntity.ok(memberRepository.findAll());
+	}
+
+	@GetMapping("/members/{id}")
+	public ResponseEntity<MemberPo> findMambersById(@PathVariable Long id) {
+		Optional<MemberPo> memberPoOpt = memberRepository.findById(id);
+		if (memberPoOpt.isEmpty()) {
+			return ResponseEntity.notFound().build();
+		} else {
+			return ResponseEntity.ok(memberPoOpt.get());
+		}
 	}
 }
